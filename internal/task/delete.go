35a78024f3c59e0512cbb26c60a31347c4408854
ArtiomLev <artiomlevshykov@gmail.com>
2026-05-29 01:36:58 +0300

    [chore]: Reformat task/delete.go

--- internal/task/delete.go
+++ internal/task/delete.go
@@ -6,10 +6,14 @@ import (
 )
 
 func (tsk *Task) Delete() error {
+	//Get path
 	taskPath := tsk.Path()
+
+	// Remove task
 	err := os.RemoveAll(taskPath)
 	if err != nil {
 		return fmt.Errorf("cannot delete task: %v", err)
 	}
+
 	return nil
 }
